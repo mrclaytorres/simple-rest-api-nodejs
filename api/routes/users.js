08acb27bdef785934580f03707f76f6cc6f8c0b7
Algen <adelacruz@innovuze.com>
2021-05-28 21:39:15 +0800

    Adding controllers to separate logic from the routes.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,107 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const checkAuth = require('../middleware/check-auth');
 
-const User = require('../models/user');
+const UsersController = require('../controllers/users');
 
-router.post('/signup', (req, res, next) => {
-    User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-        if (user.length >= 1){
-            return res.status(409).json({
-                message: 'Email already exist'
-            });
-        } else {
-            bcrypt.hash(req.body.password, 10, (err, hash) =>{
-                if (err){
-                    return res.status(500).json({
-                        error: err
-                    });
-                } else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    });
-                    user.save()
-                    .then(result => {
-                        console.log(result);
-                        res.status(201).json({
-                            message: 'User created'
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        });
-                    });
-                }
-            });
-        }
-    });
-});
+router.post('/signup', UsersController.users_signup);
 
-router.post('/login', (req, res, next) => {
-    User.find({ email: req.body.email})
-    .exec()
-    .then(user => {
-        if (user.length < 1){
-            return res.status(401).json({
-                message: 'Auth failed'
-            });
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
-            if (err){
-                res.status(401).json({
-                    message: 'Auth failed'
-                })
-            }
-            if (result){
-                const token = jwt.sign(
-                    {
-                        email: user[0].email,
-                        userId: user[0]._id
-                    },
-                    process.env.JWT_KEY,
-                    { 
-                        expiresIn: "1h" 
-                    }
-                );
+router.post('/login', UsersController.users_login);
 
-                return res.status(200).json({
-                    message: 'Auth successful',
-                    token: token
-                });
-            }
-            res.status(401).json({
-                message: 'Auth failed'
-            })
-        });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.delete('/:userId', (req, res, next) => {
-    User.deleteOne({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'User deleted'
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete('/:userId', checkAuth, UsersController.users_delete_user);
 
 module.exports = router;
